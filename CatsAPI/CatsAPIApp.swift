9de4c39c001564a4c7ff14e0b00f95b278a815c3
mcantop <maciejcantop@icloud.com>
2022-08-27 15:20:23 +0200

    Initial Commit

---
+++ CatsAPI/CatsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAPIApp.swift
+//  CatsAPI
+//
+//  Created by Maciej on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
